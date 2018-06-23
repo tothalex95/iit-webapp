462e1d8ac6ee030c87b22274cb330eb2ed473b30
Alex Toth <tothalex95@gmail.com>
2018-06-23 18:56:44 +0200

    basic routing & home component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from "@angular/core";
 import { BrowserModule } from "@angular/platform-browser";
 
 import { AppComponent } from "./app.component";
+import { AppRoutingModule } from "./app-routing.module";
+import { HomeModule } from "./home/home.module";
 
 @NgModule({
     imports: [
-        BrowserModule
+        BrowserModule,
+        AppRoutingModule,
+        HomeModule
     ],
     declarations: [
         AppComponent

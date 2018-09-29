d96e9314ae1deceb23ff42056bae2c9277284c8d
Alex Toth <tothalex95@gmail.com>
2018-09-29 14:40:59 +0200

    contact: completed

--- src/app/information/contact/contact.component.ts
+++ src/app/information/contact/contact.component.ts
@@ -4,23 +4,5 @@ import { Component } from "@angular/core";
     templateUrl: "./contact.component.html"
 })
 export class ContactComponent {
-    readonly displayedColumns: string[] = [
-        "type",
-        "value"
-    ];
-
-    readonly contacts: Array<Object> = [
-        {
-            type: "Phone",
-            value: `+36-**-***-****`
-        },
-        {
-            type: "Email",
-            value: `
-            <a href="mailto:alex.toth@iit.uni-miskolc.hu">
-                alex.toth@iit.uni-miskolc.hu
-            </a>
-            `
-        }
-    ];
+    phoneNumberVisible: boolean = false;
 }

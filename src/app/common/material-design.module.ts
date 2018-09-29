4800d779e7c2c0a3ff1ce4dae5fae4e2936b79b3
Alex Toth <tothalex95@gmail.com>
2018-09-29 16:11:27 +0200

    material: code cleanup

--- src/app/common/material-design.module.ts
+++ src/app/common/material-design.module.ts
@@ -1,69 +1,47 @@
 import { NgModule } from "@angular/core";
-import { CommonModule } from "@angular/common";
-
-import { MatTableModule } from "@angular/material/table";
-import { MatSidenavModule } from "@angular/material/sidenav";
-import { MatListModule } from "@angular/material/list";
-import { MatToolbarModule } from "@angular/material/toolbar";
-import { MatSelectModule } from "@angular/material/select";
-import { MatInputModule } from "@angular/material/input";
-import { MatPaginatorModule } from "@angular/material/paginator";
-import { MatCardModule } from "@angular/material/card";
-import { MatButtonModule } from "@angular/material/button";
-import { MatTabsModule } from "@angular/material/tabs";
-import { MatExpansionModule } from "@angular/material/expansion";
-import { MatSortModule } from "@angular/material/sort";
-import { MatButtonToggleModule } from "@angular/material/button-toggle";
-import { MatIconModule } from "@angular/material/icon";
-import { MatLineModule } from "@angular/material";
-import { MatDialogModule } from "@angular/material/dialog";
-import { MatTooltipModule } from "@angular/material/tooltip";
-import { MatGridListModule } from "@angular/material/grid-list";
-import { MatProgressBarModule } from "@angular/material/progress-bar";
+import {
+    MatButtonModule,
+    MatButtonToggleModule,
+    MatCardModule,
+    MatDialogModule,
+    MatExpansionModule,
+    MatGridListModule,
+    MatIconModule,
+    MatInputModule,
+    MatLineModule,
+    MatListModule,
+    MatPaginatorModule,
+    MatProgressBarModule,
+    MatSelectModule,
+    MatSidenavModule,
+    MatSortModule,
+    MatTableModule,
+    MatTabsModule,
+    MatToolbarModule,
+    MatTooltipModule
+} from "@angular/material";
 
 @NgModule({
-    imports: [
-        CommonModule,
-        MatTableModule,
-        MatSidenavModule,
-        MatListModule,
-        MatToolbarModule,
-        MatSelectModule,
-        MatInputModule,
-        MatPaginatorModule,
-        MatCardModule,
-        MatButtonModule,
-        MatTabsModule,
-        MatExpansionModule,
-        MatSortModule,
-        MatButtonToggleModule,
-        MatIconModule,
-        MatLineModule,
-        MatDialogModule,
-        MatTooltipModule,
-        MatGridListModule,
-        MatProgressBarModule
-    ],
     exports: [
-        MatTableModule,
-        MatSidenavModule,
-        MatListModule,
-        MatToolbarModule,
-        MatSelectModule,
-        MatInputModule,
-        MatPaginatorModule,
-        MatCardModule,
         MatButtonModule,
-        MatTabsModule,
-        MatExpansionModule,
-        MatSortModule,
         MatButtonToggleModule,
-        MatIconModule,
-        MatLineModule,
+        MatCardModule,
         MatDialogModule,
-        MatTooltipModule,
+        MatExpansionModule,
         MatGridListModule,
-        MatProgressBarModule
+        MatIconModule,
+        MatInputModule,
+        MatLineModule,
+        MatListModule,
+        MatPaginatorModule,
+        MatProgressBarModule,
+        MatSelectModule,
+        MatSidenavModule,
+        MatSortModule,
+        MatTableModule,
+        MatTabsModule,
+        MatToolbarModule,
+        MatTooltipModule
     ]
 })
 export class MaterialDesignModule { }
